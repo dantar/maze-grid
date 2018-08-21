e2ae985c593568ccae0d56d5412ebe160145447c
Daniele Tarini <d.tarini@hyperborea.com>
2018-08-21 22:16:37 +0200

    Rafactor for reuse

---
+++ src/main/java/it/dantar/games/maze/grid/MazeGridConfiguration.java
@@ -0,0 +1,10 @@
+package it.dantar.games.maze.grid;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("it.dantar.games.maze.grid")
+public class MazeGridConfiguration {
+
+}
